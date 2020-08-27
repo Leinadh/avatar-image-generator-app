6e0924bba4138eace73fa0ec23884e991eab2b2c
Leinadh <daniel.hir2@gmail.com>
2020-08-27 00:49:13 -0500

    app baseline

--- avatar-generator-app.py
+++ avatar-generator-app.py
@@ -1,5 +1,17 @@
 import streamlit as st
+from PIL import Image 
 
 st.title('Avatar Image Generator')
-st.subheader('Face  to cartoon generator')
-# st.text('') 
+st.subheader('This is a face to cartoon generator created for the Made With ML Data Science Incubator.')
+st.text('') 
+
+st.set_option('deprecation.showfileUploaderEncoding', False)
+uploaded_file = st.file_uploader('Please upload a selfie.', type=['jpg', 'jpeg'])
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Uploaded Image.', use_column_width=True)
+    st.write('')
+    st.write('Generarating Avatar...')
+    st.write('')
+    st.image(image, caption='Generated Image.', use_column_width=True)

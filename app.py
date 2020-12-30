3cd8baa2c731d36b55e98bd4c3bc3de7be996e84
Leinadh <daniel.hir2@gmail.com>
2020-12-30 14:09:38 -0500

    google analytics

--- app.py
+++ app.py
@@ -4,6 +4,27 @@ from streamlit_cropper import st_cropper
 from time import sleep
 from avatar_generator_model import Avatar_Generator_Model
 
+import re
+import os
+
+code = """<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-H5BYGHKQC6"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-H5BYGHKQC6');
+</script>"""
+
+a=os.path.dirname(st.__file__)+'/static/index.html'
+with open(a, 'r') as f:
+    data=f.read()
+    if len(re.findall('UA-', data))==0:
+        with open(a, 'w') as ff:
+            newdata=re.sub('<head>','<head>'+code,data)
+            ff.write(newdata)
+
 st.title('Avatar Image Generator')
 st.subheader("Here you can create a virtual avatar based on your appearance. Just upload a picture of your face below, crop it, and you're done!")
 st.write('')
